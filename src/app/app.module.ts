454e45d1710a167716bd917eb65816cff8c02100
imad <imad.live1990@gmail.com>
2020-04-22 20:20:18 +0200

    add routing  a project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,15 @@ import { HeaderComponent } from './header/header.component';
 import { AuthService } from './services/auth.service';
 import { BooksService } from './services/books.service';
 import { AuthGuardService } from './services/auth-guard.service';
+import {Routes, RouterModule} from '@angular/router';
+
+const appRoutes: Routes =[
+  {path: 'auth/signup', component: SignupComponent},
+  {path: 'auth/signin', component: SigninComponent},
+  {path: 'books', component: BookListComponent},
+  {path: 'books/new', component: BookFormComponent},
+  {path: 'books/view/:id', component:SingleBookComponent}
+];
 
 @NgModule({
   declarations: [
@@ -29,7 +38,7 @@ import { AuthGuardService } from './services/auth-guard.service';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService,
