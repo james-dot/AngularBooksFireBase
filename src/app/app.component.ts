fee3298cb124efa41230eb0fd388693bb1e9bcda
imad <imad.live1990@gmail.com>
2020-04-22 20:32:41 +0200

    integrer firebase au projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularBooksFireBase';
+  
+  constructor(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyBQGf9Sp1NOa9zk-_bXzVCHGGs5_xoiBxs",
+      authDomain: "angularbooksfirebase.firebaseapp.com",
+      databaseURL: "https://angularbooksfirebase.firebaseio.com",
+      projectId: "angularbooksfirebase",
+      storageBucket: "angularbooksfirebase.appspot.com",
+      messagingSenderId: "1098894371725",
+      appId: "1:1098894371725:web:d0f0b1aa5476ce8e1aabc4"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
